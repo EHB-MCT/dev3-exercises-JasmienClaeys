5cddd63cf2ff8b20347bfc348a92f1e7ae89f2fd
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-10-05 12:52:13 +0200

    win or lose

--- S1/rock paper scissors/src/main/kotlin/rock-paper-scissors.kt
+++ S1/rock paper scissors/src/main/kotlin/rock-paper-scissors.kt
@@ -1,14 +1,41 @@
 fun main() {
-    val options = arrayOf("Rock", "Paper", "Scissors")
-    val randomNumber = (0..2).random()
+    val options = arrayOf("rock", "paper", "scissors")
+    val computerChoice = options.random()
 
-    print("Rock, paper, scissors?")
+    print("rock, paper, scissors?")
 
-    val answer = readLine()
-    println("Your choice: $answer")
-    println(options[randomNumber])
+    val userChoice = readLine()
+    println("Computer choice: $computerChoice")
+    println("Your choice: $userChoice")
 
-    if (answer == "Paper") {
+    if (computerChoice == userChoice) {
+        println("it's a draw")
+    } else {
+        if(userChoice == "paper"){
+            if (computerChoice == "rock") {
+                println("you win")
+            }
+            if(computerChoice == "scissors") {
+                println("you lose")
+            }
+        }
 
+        if(userChoice == "rock"){
+            if (computerChoice == "paper") {
+                println("you lose")
+            }
+            if(computerChoice == "scissors") {
+                println("you win")
+            }
+        }
+
+        if(userChoice == "scissors"){
+            if (computerChoice == "rock") {
+                println("you lose")
+            }
+            if(computerChoice == "paper") {
+                println("you win")
+            }
+        }
     }
 }
