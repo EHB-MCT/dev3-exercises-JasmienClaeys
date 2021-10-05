eed2244b85b192da83b826ace9480fbdd47b702f
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-10-05 12:31:25 +0200

    if

--- S1/rock paper scissors/src/main/kotlin/rock-paper-scissors.kt
+++ S1/rock paper scissors/src/main/kotlin/rock-paper-scissors.kt
@@ -2,12 +2,13 @@ fun main() {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val randomNumber = (0..2).random()
 
-    println(options[randomNumber])
-
-
     print("Rock, paper, scissors?")
 
     val answer = readLine()
     println("Your choice: $answer")
+    println(options[randomNumber])
 
+    if (answer == "Paper") {
+
+    }
 }
