2d036ead700c3bbd9850aef613233ff9872a06e9
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-10-19 11:13:21 +0200

    chatroullette opstart deel 2

--- S3/ChatRoullete-2/src/main/kotlin/main.kt
+++ S3/ChatRoullete-2/src/main/kotlin/main.kt
@@ -6,14 +6,10 @@ fun main(){
     println("Player 2, what's your name?")
     val person2 = readLine()
     val name2 = Person("$person2")
-
-//    val person3 = Person("Hector")
-//    val person4 = Person("Ella")
-//    val persons = arrayOf(person3.name, person4.name)
+    
     val persons = arrayOf(name1.name, name2.name)
 
     val pickPerson = persons.random()
 
     println("Hi there my name is $pickPerson")
-//    Person.introduce(pickPerson)
 }
