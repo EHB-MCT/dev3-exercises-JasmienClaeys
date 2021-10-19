f84d6ce08d19d482463101e3fddf9aefb4d9d0c3
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-10-19 11:09:08 +0200

    input name + introduce

--- S3/ChatRoullette/src/main/kotlin/Person.kt
+++ S3/ChatRoullette/src/main/kotlin/Person.kt
@@ -1,2 +1,5 @@
 open class Person(open val name: String) {
+//    fun introduce(
+//        println("Hi, my name is $name")
+//    )
 }
--- S3/ChatRoullette/src/main/kotlin/main.kt
+++ S3/ChatRoullette/src/main/kotlin/main.kt
@@ -1,9 +1,19 @@
 fun main(){
-//    val person1 = Person("Person1")
-//    val person2 = Person("Person2")
-    val persons = arrayOf(Person("Person1"), Person("Person2"))
+    println("Player 1, what's your name?")
+    val person1 = readLine()
+    val name1 = Person("$person1")
+
+    println("Player 2, what's your name?")
+    val person2 = readLine()
+    val name2 = Person("$person2")
+
+    val person3 = Person("Hector")
+    val person4 = Person("Ella")
+//    val persons = arrayOf(person3.name, person4.name)
+    val persons = arrayOf(name1.name, name2.name)
 
     val pickPerson = persons.random()
 
-    println("Hi, welcome my name is $pickPerson")
+    println("Hi there my name is $pickPerson")
+//    Person.introduce(pickPerson)
 }
