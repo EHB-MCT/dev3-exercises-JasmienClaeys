39d6ddd05feb1f611b78455f7ec330374d6b87f5
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-10-19 10:24:37 +0200

    demo

--- S3/OOP/src/main/kotlin/main.kt
+++ S3/OOP/src/main/kotlin/main.kt
@@ -1,12 +1,22 @@
+//fun main(){
+//    val design1 = Course("design1",
+//        1,
+//        5,
+//        "a fine course",
+//        "Maaike Beuten",
+//        1,
+//        "Design",
+//        true
+//    )
+//
+//}
+
 fun main(){
-    val design1 = Course("design1",
-        1,
-        5,
-        "a fine course",
-        "Maaike Beuten",
-        1,
-        "Design",
-        true
-    )
-    
+    val dries = Hero("Dries")
+    val jens = Hero("Jens")
+    val matthias = Villain("Matthias")
+
+    dries.talk()
+    jens.talk()
+    matthias.talk()
 }
