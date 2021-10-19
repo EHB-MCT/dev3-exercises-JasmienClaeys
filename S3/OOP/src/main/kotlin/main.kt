bc2d90e0268236acacd7bd8b235feb8e6294eb4a
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-10-19 09:44:44 +0200

    course aanmaken

---
+++ S3/OOP/src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val design1 = Course("design1",
+        1,
+        5,
+        "a fine course",
+        "Maaike Beuten",
+        1,
+        "Design",
+        true
+    )
+    
+}
