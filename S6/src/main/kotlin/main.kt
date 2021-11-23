d735c3b5117d781333c94298c0458b1b8e81deb9
JasmienClaeys <jasmien.claeys@student.ehb.be>
2021-11-23 12:42:42 +0100

    transport part 1: result weergeven nog fout

---
+++ S6/src/main/kotlin/main.kt
@@ -0,0 +1,41 @@
+import java.sql.*
+import java.util.*
+
+fun  main() {
+    Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance()
+
+    val credentials = Credentials()
+
+    // Prepare credentials
+    val connectionProps = Properties()
+    connectionProps["user"] = credentials.databaseUser
+    connectionProps["password"] =  credentials.databasePassword
+
+    // Create the connection: this will allow us to run queries on it later
+    val connection =  DriverManager.getConnection(
+        "jdbc:" + "mysql" + "://" +
+                credentials.databaseHost +
+                ":" + "3306" + "/" +
+                credentials.databaseName,
+        connectionProps)
+
+    println("Choose your destination city: Brussel or Denderleeuw or Ternat")
+    val search = readLine()
+
+    val statement = connection.prepareStatement("SELECT * FROM S6_trains" +
+            "INNER JOIN S6_rides ON S6_trains.id = S6_rides.train_id" +
+            "WHERE departure_city = ?")
+    // The result of your query will be a type ResultSet
+    // But since the library is based on Java, we get a type from Java
+    statement.setString(1, search)
+    // search verwijst naar de variabele: readline van de user en deze info "beveiligd" ingeven
+    val result = statement.executeQuery()
+    while(result.next()) {
+        val city1 = result.getString("departure_city")
+        println(city1)
+    }
+
+
+
+    //SELECT * FROM trains
+}
